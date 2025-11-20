c00c95eb11bfe03b499ab9ee6dca115498c94eed
A00625466 <A00625466@5QWJ724>
2025-11-20 11:26:47 -0600

    added second player and collisions with border

--- drivingGame/drivingGame.cpp
+++ drivingGame/drivingGame.cpp
@@ -66,6 +66,45 @@ void handleInputandLogic(car& playercar, float deltaTime)
 	playercar.positionY += playercar.velocityY * deltaTime;
 }
 
+void handleInputandLogicP2(car& playercar, float deltaTime)
+{
+	SDL_PumpEvents();
+	const bool* keystate = SDL_GetKeyboardState(NULL);
+
+	float radians = playercar.rotation * M_PI / 180.0f;
+	SDL_FPoint direction = { cos(radians), sin(radians) };
+
+	float ROTATION_SPEED = 0.0005 * (sqrt(pow(playercar.velocityX, 2) + pow(playercar.velocityY, 2)));
+
+	if (keystate[SDL_SCANCODE_A])
+	{
+		playercar.rotation -= ROTATION_SPEED;
+	}
+	else if (keystate[SDL_SCANCODE_D])
+	{
+		playercar.rotation += ROTATION_SPEED;
+	}
+	if (keystate[SDL_SCANCODE_W])
+	{
+		playercar.velocityX += direction.x * ACCELERATION * deltaTime;
+		playercar.velocityY += direction.y * ACCELERATION * deltaTime;
+	}
+	else if (keystate[SDL_SCANCODE_S])
+	{
+		playercar.velocityX -= direction.x * ACCELERATION * deltaTime;
+		playercar.velocityY -= direction.y * ACCELERATION * deltaTime;
+	}
+
+
+	float DRAG_FACTOR = 0.999f;
+	playercar.velocityX *= DRAG_FACTOR;
+	playercar.velocityY *= DRAG_FACTOR;
+
+
+	playercar.positionX += playercar.velocityX * deltaTime;
+	playercar.positionY += playercar.velocityY * deltaTime;
+}
+
 void logSDLError(const std::string& msg) {
 	SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "%s error: %s\n", msg.c_str(), SDL_GetError());
 }
@@ -97,7 +136,7 @@ bool loadMedia() {
 	bool success = true;
 
 	// Load background texture
-	// *** CRITICAL: Update this path to your actual BMP file location! ***
+	
 	texture = loadTexture("track.bmp");
 
 	if (texture == nullptr) {
@@ -112,7 +151,7 @@ int main()
 
 
 	car player;
-
+	car player2;
 	
 	
 	window = SDL_CreateWindow("Driving", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
@@ -134,12 +173,21 @@ int main()
 		player.playerTexture = loadTexture("car.bmp");
 
 		// Initialize position/size ONCE
-		player.position = { 500.0f, 500.0f, 25.0f, 10.0f };
+		player.position = { 500.0f, 490.0f, 25.0f, 10.0f };
 		player.positionX = player.position.x;
 		player.positionY = player.position.y;
 		player.rotation = 0.0;
 		player.velocityX = 0.0;
 		player.velocityY = 0.0;
+
+		player2.playerTexture = loadTexture("car2.bmp");
+
+		player2.position = { 400.0f, 400.0f, 25.0f, 10.0f };
+		player2.positionX = player2.position.x;
+		player2.positionY = player2.position.y;
+		player2.rotation = 0.0;
+		player2.velocityX = 0.0;
+		player2.velocityY = 0.0;
 	}
 	else {
 		// Handle media loading failure
@@ -148,10 +196,11 @@ int main()
 		SDL_Quit();
 		return 1;
 	}
-	SDL_Surface* backgroundSurface = SDL_LoadBMP("track.bmp");
+	
+	//this is giving me collision with the window boundaries but not the texture color because it reads the renderer as just the window.
+	SDL_Surface* backgroundSurface = SDL_RenderReadPixels(renderer, nullptr);
 
-	SDL_Color color = { 34, 177, 76 };
-	Uint32 collisionColor = SDL_MapRGB( backgroundSurface->format, color.r, color.g, color.b );
+	
 
 	auto last_time = std::chrono::high_resolution_clock::now();
 	bool running = true;
@@ -164,8 +213,10 @@ int main()
 
 		double nextX = player.positionX + player.velocityX;
 		double nextY = player.positionY + player.velocityY;
+		double nextX2 = player2.positionX + player2.velocityX;
+		double nextY2 = player2.positionY + player2.velocityY;
 
-		if (!checkCollision (nextX, nextY, backgroundSurface, collisionColor))
+		
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_EVENT_QUIT)
@@ -177,11 +228,38 @@ int main()
 		player.position.x = (float)player.positionX;
 		player.position.y = (float)player.positionY;
 
+		handleInputandLogicP2(player2, deltaTime);
+		player2.position.x = (float)player2.positionX;
+		player2.position.y = (float)player2.positionY;
+
+		//check collision between players
+		if (std::abs(player.positionX - player2.positionX) < 15 && std::abs(player.positionY - player2.positionY) < 15)
+		{
+			double vX = player.velocityX;
+			double vY = player.velocityY;
+			player.velocityX = player2.velocityX;
+			player.velocityY = player2.velocityY;
+			player2.velocityX = vX;
+			player2.velocityY = vY;
+		}
+		
+		if (checkCollision(player.positionX, player.positionY, backgroundSurface, 0x00FF00))
+		{
+			player.velocityX = player.velocityX * (-2);
+			player.velocityY = player.velocityY * (-2);
+		}
+		if (checkCollision(player2.positionX, player2.positionY, backgroundSurface, 0x00FF00))
+		{
+			player2.velocityX = player2.velocityX * (-2);
+			player2.velocityY = player2.velocityY * (-2);
+		}
 		SDL_RenderClear(renderer);
 		SDL_RenderTexture(renderer, texture, nullptr, nullptr);
 
 		SDL_RenderTextureRotated(renderer, player.playerTexture, nullptr, &player.position,
 			player.rotation, nullptr, SDL_FLIP_NONE);
+
+		SDL_RenderTextureRotated(renderer, player2.playerTexture, nullptr, &player2.position, player2.rotation, nullptr, SDL_FLIP_NONE);
 
 		SDL_RenderPresent(renderer);
 	}
